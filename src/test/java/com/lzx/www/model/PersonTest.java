ecf043f04b72694a7044855c50bbf63212c70f3d
lzx2619263 <lzx2619263@163.com>
2019-08-19 19:34:21 +0800

    commit;

--- src/test/java/com/lzx/www/model/PersonTest.java
+++ src/test/java/com/lzx/www/model/PersonTest.java
@@ -10,7 +10,7 @@ public class PersonTest {
 	@Test
 	public void test() {
 		Person p = new Person();
-		p.request("http://10.23.223.151:8080");
+		p.request("http://10.21.127.63:8080");
 		System.out.println("aaa");
 	}
 
